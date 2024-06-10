203cdee82703c3bbbe9052d9d84e1fdb6b4a3da6
JosephFer <ferrerjoseph04@gmail.com>
2024-06-10 17:51:48 -0500

    modificación del lexer

--- src/lexer.py
+++ src/lexer.py
@@ -1,73 +1,92 @@
-import re
+import ply.lex as lex
 
-class Lexer(object):
-    def __init__(self, sourceCode):
-        self.sourceCode = sourceCode
-        self.tokens = [ 
-          #palabras resevadas
-          ('T_IF', r'ifisinho'),
-          ('T_ELIF', r'elifsinho'),
-          ('T_ELSE', r'elsinho'),
-          ('T_WHILE', r'darMole'),
-          ('T_BREAK', r'daStop'),
-          ('T_RETURN', r'volte'),
-          ('T_PRINT', r'imprime'),
+# Definición de tokens
+tokens = [
+    'T_IF', 'T_ELIF', 'T_ELSE', 'T_WHILE', 'T_FOR', 'T_IN', 'T_RANK', 'T_DO','T_BREAK', 'T_RETURN', 'T_PRINT',
+    'T_ENTERO', 'T_FLOTANTE', 'T_BOOL', 'T_CADENA', 'T_TRUE', 'T_FALSE',
+    'T_VARIABLE', 'T_NUMERO', 'T_SUMA', 'T_RESTA', 'T_MULTIPLICACION', 'T_DIVISION',
+    'T_AND', 'T_OR', 'T_NOT', 'T_MAYORQUE', 'T_MENORQUE', 'T_MAYOROIGUAL', 'T_MENOROIGUAL',
+    'T_DIFERENTE', 'T_IGUAL', 'T_ASIGNACION', 'T_COMA', 'T_FINDELINEA', 'T_LLAVEDERECHA',
+    'T_LLAVEIZQUIERDA', 'T_PDERECHA', 'T_PIZQUIERDA', 'T_STRING', 'T_FUNCION'
+]
 
-          #tipos de datos, 
-          ('T_ENTERO', r'intnho'),
-          ('T_FLOTANTE', r'floatinho'),
-          ('T_BOOL', r'boolinho'),
-          ('T_CADENA', r'stringho'),
-          ('T_TRUE', r'VERDA'),
-          ('T_FALSE', r'FALSO'),
-          ('T_VARIABLE', r'\$[a-zA-Z0-9_]+'),
-          ('T_NUMERO', r"\d+"),
+# Definición de las expresiones regulares para los tokens
+t_T_IF = r'ifisinho'
+t_T_ELIF = r'elifsinho'
+t_T_ELSE = r'elsinho'
+t_T_WHILE = r'darMole'
+t_T_FOR = r'faca'
+t_T_IN = r'in'
+t_T_RANK = r'rank'
+t_T_DO = r'facaMole'
+t_T_BREAK = r'daStop'
+t_T_RETURN = r'volte'
+t_T_PRINT = r'imprime'
+t_T_ENTERO = r'intnho'
+t_T_FLOTANTE = r'floatinho'
+t_T_BOOL = r'boolinho'
+t_T_CADENA = r'stringho'
+t_T_TRUE = r'verda'
+t_T_FALSE = r'falso'
+t_T_VARIABLE = r'\$[a-zA-Z0-9_]+'
+t_T_NUMERO = r'\d+'
+t_T_SUMA = r'\+'
+t_T_RESTA = r'-'
+t_T_MULTIPLICACION = r'\*'
+t_T_DIVISION = r'/'
+t_T_AND = r'AND'
+t_T_OR = r'OR'
+t_T_NOT = r'NOT'
+t_T_MAYORQUE = r'>'
+t_T_MENORQUE = r'<'
+t_T_MAYOROIGUAL = r'>='
+t_T_MENOROIGUAL = r'<='
+t_T_DIFERENTE = r'!='
+t_T_IGUAL = r'=='
+t_T_ASIGNACION = r'='
+t_T_COMA = r','
+t_T_FINDELINEA = r';'
+t_T_LLAVEDERECHA = r'}'
+t_T_LLAVEIZQUIERDA = r'{'
+t_T_PDERECHA = r'\('
+t_T_PIZQUIERDA = r'\)'
+t_T_STRING = r'\".*?\"'
+t_T_FUNCION = r'\#[a-zA-Z_][a-zA-Z0-9_]*'
 
-          #Operadores matemáticos 
-          ('T_SUMA', r'\+'),
-          ('T_RESTA', r'\-'),
-          ('T_MULTIPLICACION', r'\*'),
-          ('T_DIVISION', r"/"),
-          
-          #operadores logicos
-          ('T_AND', r'&&'),
-          ('T_OR', r'\|\|'),
-          ('T_NOT', r'![a-zA-Z0-9_]+'),
+# Ignorar espacios y tabulaciones
+t_ignore = ' \t'
 
-          #Simbologia del lenguaje
-          ('T_MAYORQUE', r'>'),
-          ('T_MENORQUE', r'<'),
-          ('T_MAYOROIGUAL', r'>='),
-          ('T_MENOROIGUAL', r'<='),
-          ('T_DIFERENTE', r'!='),
-          ('T_IGUAL', r'=='),
-          ('T_FINDELINEA', r';'),
-          ('T_LLAVEDERECHA', r'{'),
-          ('T_COMA', r','),
-          ('T_LLAVEIZQUIERDA', r'}'),
-          ('T_PDERECHA', r'\('),
-          ('T_PIZQUIERDA', r'\)'),          
-          #
-          ('T_ASIGNACION', r'='),
-          
-          ('T_FUNCION', r'\#[a-zA-Z_][a-zA-Z0-9_]*'),
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
-        ]
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
 
-    def tokenizar(self):
-        tokensEncontrados = []
-        
-        #
-        palabras = re.findall(r'\$\w+|\#\w+|\w+|\S', self.sourceCode)
-        
-        for palabra in palabras:
-            for token, patron in self.tokens:
-                if re.fullmatch(patron, palabra):
-                    tokensEncontrados.append((palabra, token))
-                    break
-            else:
-                # Si ninguna expresión regular coincide con la palabra, la consideramos un token desconocido
-                tokensEncontrados.append((palabra, 'T_DESCONOCIDO'))
+# Construcción del lexer
+lexer = lex.lex()
 
-        
-        return tokensEncontrados
+
+content = """
+	boolinho $variable = verda;
+    darMole ($variable) {
+		$variable = falso;
+        imprime("hola");
+    };
+    
+    faca $numero in rank(0, 10, 1){
+		imprime("hola);
+    };
+    
+    $i = 0;
+    facaMole {
+    	imprime($i);
+        $i = $i + 1;
+        } darMole($i < 5);
+
+   """
+
+lexer.input(content)
+for token in lexer:
+    print(f"Token: {token.type}, Valor: {token.value}, Línea: {token.lineno}")
